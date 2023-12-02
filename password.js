5324ad8a9d826137593d08cb15cd8d5dfa1dcc95
Aingaran <email@aingaran.com>
2023-12-01 19:32:55 -0500

    added copy button

--- password.js
+++ password.js
@@ -1,31 +1,31 @@
-let message = "";
-let length = 16;
-const messageDiv = document.querySelector("#message");
+let message = ""
+let length = 16
+const messageDiv = document.querySelector("#message")
 
 const sliding = () => {
-  length = document.querySelector("#length").value;
-  pressed();
+  length = document.querySelector("#length").value
+  pressed()
 }
 
 const pressed = () => {
-  message = "";
-  let randomPassword = '';
-  let chars = "";
+  message = ""
+  let randomPassword = ""
+  let chars = ""
 
-  let length = null;
-  let caps = null;
-  let special = null;
-  let numbers = null;
-  let lowercase = null;
+  let length = null
+  let caps = null
+  let special = null
+  let numbers = null
+  let lowercase = null
 
-  length = document.querySelector("#length").value;
-  caps = document.querySelector("#caps").checked;
-  special = document.querySelector("#special").checked;
-  numbers = document.querySelector("#numbers").checked;
-  lowercase = document.querySelector("#lowercase").checked;
+  length = document.querySelector("#length").value
+  caps = document.querySelector("#caps").checked
+  special = document.querySelector("#special").checked
+  numbers = document.querySelector("#numbers").checked
+  lowercase = document.querySelector("#lowercase").checked
 
   if (lowercase) {
-    chars += "abcdefghijklmnopqrstuvwxyz";
+    chars += "abcdefghijklmnopqrstuvwxyz"
   }
 
   if (caps) {
@@ -41,36 +41,52 @@ const pressed = () => {
   }
 
   if (!chars) {
-    chars = "";
-    message = "one of the options must be selected";
+    chars = ""
+    message = "one of the options must be selected"
   }
 
-  let charactersLength = chars.length;
+  let charactersLength = chars.length
   for (let i = 0; i < length; i++) {
-    randomPassword += chars.charAt(Math.floor(Math.random() * charactersLength));
-
+    randomPassword += chars.charAt(Math.floor(Math.random() * charactersLength))
   }
-  document.querySelector("#password").value = randomPassword;
-  document.querySelector("#pwlength").innerText = length;
-  navigator.clipboard.writeText(randomPassword);
-  messageDiv.innerText = message;
-  return randomPassword;
-
+  document.querySelector("#password").value = randomPassword
+  document.querySelector("#pwlength").innerText = length
+  navigator.clipboard.writeText(randomPassword)
+  messageDiv.innerText = message
+  return randomPassword
 }
 
-const lowercaseCheckbox = document.getElementById("lowercase").addEventListener("input", pressed);
+const lowercaseCheckbox = document
+  .getElementById("lowercase")
+  .addEventListener("input", pressed)
 
-const capsCheckbox = document.getElementById("caps").addEventListener("input", pressed);
+const capsCheckbox = document
+  .getElementById("caps")
+  .addEventListener("input", pressed)
 
-const numbersCheckbox = document.getElementById("numbers").addEventListener("input", pressed);
+const numbersCheckbox = document
+  .getElementById("numbers")
+  .addEventListener("input", pressed)
 
-const specialCheckbox = document.getElementById("special").addEventListener("input", pressed);
+const specialCheckbox = document
+  .getElementById("special")
+  .addEventListener("input", pressed)
 
-const slider = document.getElementById("length").addEventListener("input", pressed);
+const slider = document
+  .getElementById("length")
+  .addEventListener("input", pressed)
 
-const startButton = document.getElementById('button-generate-password').addEventListener('click', pressed);
+const startButton = document
+  .getElementById("button-generate-password")
+  .addEventListener("click", pressed)
+
+const copyButton = document
+  .getElementById("button-copy-password")
+  .addEventListener("click", () => {
+    navigator.clipboard.writeText(document.querySelector("#password").value)
+  })
 
 window.addEventListener("load", () => {
-  document.querySelector("#pwlength").innerText = length;
-  pressed();
-});
+  document.querySelector("#pwlength").innerText = length
+  pressed()
+})
